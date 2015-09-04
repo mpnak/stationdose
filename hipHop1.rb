4c281323fbe9ea94ffed0ae7e78b2c680f722aa1
Vaughan Kelly Guy <vkellyguy@gmail.com>
2015-09-03 21:22:15 -0400

    fixes. Notes in file comments

--- hipHop1.rb
+++ hipHop1.rb
@@ -1,13 +1,37 @@
 require 'json'
 require 'bundler'
-require 'echowrap'
 Bundler.require
 
+# You have the 'config' stored in a seperate file but it is not loaded automatically. We have to explicity load that file here:
+require_relative 'config/initializers/echowrap'
 
 
+# Lets see what taste profiles exists?
+#ap Echowrap.taste_profile_list
 
-hipHopProfileId = 'AVPFPD14F9486F72B'
+# Nice, the hipHopProfile1 already exists.
 
-Echowrap.taste_profile_update(:id => 'hipHopProfileId', :data=> 'fileHipHop1').inspect
+# It looks like you had the wrong id for the hipHopProfile1 ...
+# You had:
+#   hipHopProfileId = 'AVPFPD14F9486F72B'
+# But compare this to what you see when you run:
+#   ap Echowrap.taste_profile_list
+# So lets set the correct ID
+hipHopProfileId = 'CAVPFPD14F9486F72B'
 
-puts Echowrap.taste_profile_read(:id => 'hipHopProfileId').inspect
+# Note, a couple of changes of the next line
+# for reference, your attempt was this:
+#       Echowrap.taste_profile_update(:id => 'hipHopProfileId', :data=> 'fileHipHop1')
+
+# First we read the contents of the file
+file_contents = File.read('public/hipHip1.json')
+
+# A quick look to see if the data has loaded correctly (always good to check these things):
+#ap file_contents
+
+# Now we are ready to update
+Echowrap.taste_profile_update(:id => hipHopProfileId, :data => file_contents)
+
+# Notice that hipHopProfileId is a variable that contains the value of 'CAVPFPD14F9486F72B' which is encased in quotation '' marks. When we reference hipHopProfileId we don't quote it.
+
+ap Echowrap.taste_profile_read(:id => hipHopProfileId)
