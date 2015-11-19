d217966ecd654746dae59fde9b6d8717ce274d3a
Developer <developer@trygigster.com>
2015-11-19 18:24:00 -0300

    New spotify credentials

--- Stationdose/Classes/Constants.swift
+++ Stationdose/Classes/Constants.swift
@@ -10,10 +10,12 @@ import Foundation
 
 struct Constants {
     struct Spotify {
-        static let ClientId = "0b23eb44f6d345c998dbb61766a35e8d"
-        static let ClientSecret = "91980d6e26fe451496a1ffc5b7305662"
-        static let RedirectUrl = "stationdose-app-login://"
+        static let ClientId = "dcc5c23ee5e94b6c9583667061a37913"
+        static let ClientSecret = "5affe6aadabb400aafe1115ebe59528a"
+        static let RedirectUrl = "stationdose-login://callback"
+        /*
         static let LoginUrl =  NSURL(string:"https://accounts.spotify.com/en/login?continue=https:%2F%2Faccounts.spotify.com%2Fen%2Fauthorize%3Fclient_id%3D"+ClientId+"%26scope%3Dstreaming%20user-read-private%26redirect_uri%3Dstationdose-app-login:%252F%252F%26nosignup%3Dfalse%26nolinks%3Dfalse%26response_type%3Dtoken")
+        */
         static let GoPremiumUrl = NSURL(string:"https://www.spotify.com/premium")
     }
     struct Notifications{
